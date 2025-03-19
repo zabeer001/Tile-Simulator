d42f273ebec5a6e8ab00e755ed041ea10d2dc325
Kongkon-79 <kongkonjowarder@gmail.com>
2025-03-19 16:08:24 +0600

    add new tiles form add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         {children}
+        <Toaster position="top-right" />
       </body>
     </html>
   );
